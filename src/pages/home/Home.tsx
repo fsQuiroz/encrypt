2925ab66768dede79bb66d69f3cf50c4b0f89056
Juan <jmponceguajardo@gmail.com>
2023-11-14 17:08:32 -0300

    se cambia box por grid en Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,90 +1,92 @@
-import { Box, Container, TextField, Typography } from '@mui/material';
 import { FunctionComponent } from 'react';
-import EncryptButton from '../../components/encryptButton/EncryptButton';
-import CopyButton from '../../components/copyButton/CopyButton';
-import DecryptButton from '../../components/decryptButton/DecryptButton';
-import ShareButton from '../../components/shareButton/ShareButton';
+import { Container, Grid, TextField, Typography } from '@mui/material';
+import EncryptButton from '../../components/encryptButton/EncryptButton.tsx';
+import DecryptButton from '../../components/decryptButton/DecryptButton.tsx';
+import CopyButton from '../../components/copyButton/CopyButton.tsx';
+import ShareButton from '../../components/shareButton/ShareButton.tsx';
 
-const Home: FunctionComponent = () => {
+const GridHome: FunctionComponent = () => {
   return (
-    <Container
-      maxWidth="md"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        alignContent: 'center',
-      }}>
-      <Box>
-        <Typography variant="h3" margin={'2rem'}>
-          Encrypt
-        </Typography>
-      </Box>
-      <Box sx={{ width: '80%' }}>
-        <TextField
-          label="Texto"
-          variant="outlined"
-          helperText="Ingresa el texto que deseas encriptar/desencriptar"
-          id="text"
-          multiline
-          minRows={10}
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: 'white',
+    <Container maxWidth="sm">
+      <Grid container direction="column" justifyContent="flex-start" alignItems="stretch" spacing={2}>
+        <Grid item md={12}>
+          <Typography variant="h3" align="center">
+            Encrypt
+          </Typography>
+        </Grid>
+        <Grid item md={12}>
+          <TextField
+            label="Text"
+            variant="outlined"
+            helperText="Ingresa el texto que deseas encriptar/desencriptar"
+            multiline
+            minRows={10}
+            maxRows={10}
+            fullWidth
+            InputProps={{
+              style: {
+                color: 'white',
               },
-              '&:hover fieldset': {
-                borderColor: 'white',
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'white',
+                },
+                '&:hover fieldset': {
+                  borderColor: 'white',
+                },
               },
-            },
-            '& .MuiFormHelperText-root': {
-              color: 'white',
-            },
-            '& .MuiFormLabel-root': {
-              color: 'white',
-            },
-            width: '100%',
-            margin: '1rem',
-          }}
-        />
-      </Box>
-
-      <Box>
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          helperText="Aquí debes poner tu contraseña para encriptar/desencriptar"
-          id="textEncrypt"
-          type="password"
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: 'white',
+              '& .MuiFormHelperText-root': {
+                color: 'white',
               },
-              '&:hover fieldset': {
-                borderColor: 'white',
+              '& .MuiFormLabel-root': {
+                color: 'white',
               },
-            },
-            '& .MuiFormHelperText-root': {
-              color: 'white',
-            },
-            '& .MuiFormLabel-root': {
-              color: 'white',
-            },
-            mt: 2,
-            width: '15rem',
-          }}></TextField>
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <EncryptButton />
-        <DecryptButton />
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <CopyButton />
-        <ShareButton />
-      </Box>
+            }}
+          />
+        </Grid>
+        <Grid item md={12}>
+          <TextField
+            label="Contraseña"
+            variant="outlined"
+            helperText="Aquí debes poner tu contraseña para encriptar/desencriptar"
+            type="password"
+            fullWidth
+            InputProps={{
+              style: {
+                color: 'white',
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'white',
+                },
+                '&:hover fieldset': {
+                  borderColor: 'white',
+                },
+              },
+              '& .MuiFormHelperText-root': {
+                color: 'white',
+              },
+              '& .MuiFormLabel-root': {
+                color: 'white',
+              },
+            }}
+          />
+        </Grid>
+        <Grid item md={12} m="auto">
+          <EncryptButton />
+          <DecryptButton />
+        </Grid>
+        <Grid item md={12} m="auto">
+          <CopyButton />
+          <ShareButton />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
 
-export default Home;
+export default GridHome;
