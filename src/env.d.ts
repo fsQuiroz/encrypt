5cec58174be1d71616770a40d84cac456a45f87d
Francisco Sáenz <franjsaenz@gmail.com>
2024-05-23 21:22:39 -0300

    Add Encryption Service

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_VERSION: string;
+  readonly VITE_BASE_URL: string;
   readonly VITE_ENCRYPTED_QUERY_PARAM: string;
   readonly VITE_IV_SIZE: number;
 }
---
+++ src/service/encryption.service.ts
@@ -0,0 +1,45 @@
+import { lib, enc, SHA256, AES } from 'crypto-js';
+
+const ivSize: number = import.meta.env.VITE_IV_SIZE;
+
+export const encrypt: (note: string, pass: string) => string = (note: string, pass: string) => {
+  // Generate encryption key from input passphrase
+  const secretKey = SHA256(pass);
+  // Generate initialization vector
+  const iv = lib.WordArray.random(ivSize);
+
+  // Encrypt input note
+  const encryptedNote = AES.encrypt(note, secretKey, { iv: iv }).toString();
+
+  // Package encryption information
+  const pkg = {
+    encrypted: encryptedNote,
+    iv: enc.Base64.stringify(iv),
+  };
+
+  // Stringify and Base64 encode package
+  return btoa(JSON.stringify(pkg));
+};
+
+export const decrypt: (note: string, pass: string) => string | null = (note: string, pass: string) => {
+  // Parse and Base64 decode package with encryption information
+  const pkg = {
+    encrypted: '',
+    iv: '',
+  };
+  try {
+    const pkgTmp = JSON.parse(atob(note));
+    pkg.encrypted = pkgTmp.encrypted;
+    pkg.iv = pkgTmp.iv;
+  } catch (e) {
+    return null;
+  }
+
+  // Generate encryption key from input passphrase
+  const secretKey = SHA256(pass);
+  // Get initialization vector from package
+  const iv = enc.Base64.parse(pkg.iv);
+
+  // Decrypt input note
+  return AES.decrypt(pkg.encrypted, secretKey, { iv: iv }).toString(enc.Utf8);
+};
